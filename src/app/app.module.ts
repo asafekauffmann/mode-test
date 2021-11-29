3d161004124855e1b09712dc5e11d785680028b3
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-29 15:17:46 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DarkToggleComponent } from './dark-toggle/dark-toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkToggleComponent
   ],
   imports: [
     BrowserModule,
